a817315a5433f921578052d188ac12ab3c0f2c08
gabrielsantos89 <86385613+gabrielsantos89@users.noreply.github.com>
2021-07-25 01:52:23 -0300

    1.0.1

--- projeto/test/widget_test.dart
+++ projeto/test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:projeto_avaliativo/LayoutCard.dart';
 
 import 'package:projeto_avaliativo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(LayoutCard());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
